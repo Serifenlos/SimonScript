f241825684cfd5c61f6d44e2377f740fdf9998a5
Simon Adrian <simonadrian@gmx.de>
2020-03-28 22:47:00 +0100

    build

---
+++ source/A__psScripts/test__dev.jsx
@@ -0,0 +1,7 @@
+//@include "functions/basic.jsx";
+//@include "functions/LUT-dodge.jsx";
+//@include "functions/LUT-burn.jsx";
+//@include "functions/zoom.jsx";
+
+app.activeDocument.suspendHistory('Burn', 'LUT_burn();');
+app.activeDocument.suspendHistory('Dodge', 'LUT_dodge();');
